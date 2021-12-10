95a94baaf8f0b5c26a5afa12d7e0ab438af9dc39
Venom207 <62240450+Venom207@users.noreply.github.com>
2021-12-10 19:09:56 +0300

    update package.json, wdio.config

--- wdio.conf.js
+++ wdio.conf.js
@@ -97,7 +97,7 @@ exports.config = {
   // with `/`, the base url gets prepended, not including the path portion of your baseUrl.
   // If your `url` parameter starts without a scheme or `/` (like `some/path`), the base url
   // gets prepended directly.
-  baseUrl: "http://localhost", // https://enduring.herokuapp.com/login
+  baseUrl: "https://enduring.herokuapp.com/login",
   //
   // Default timeout for all waitFor* commands.
   waitforTimeout: 10000,
@@ -135,7 +135,15 @@ exports.config = {
   // Test reporter for stdout.
   // The only one supported by default is 'dot'
   // see also: https://webdriver.io/docs/dot-reporter
-  reporters: ["spec"],
+  reporters: [
+    "spec",
+    [
+      "allure",
+      {
+        outputDir: "allure-results",
+      },
+    ],
+  ],
 
   //
   // Options to be passed to Mocha.
